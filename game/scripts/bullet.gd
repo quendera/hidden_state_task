85519e0969246951e8ff8f77dcc0f7800ce8961d
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2017-05-24 16:02:22 +0100

    Simplify bullet

--- game/scripts/bullet.gd
+++ game/scripts/bullet.gd
@@ -2,17 +2,12 @@ extends Area2D
 
 const SPEED = 600
 var dir
-var polarity
-var visible_color
+var polarity # 0 is red, 1 is blue
 var targets
 var super
 
 func _process(delta):
-	if polarity == 0:
-		visible_color = "red"
-	else:
-		visible_color = "blue"
-	set_color(visible_color)
+	get_node("frames").set_frame(polarity)
 	translate(delta*SPEED*dir)
 
 func _ready():
@@ -29,12 +24,7 @@ func _on_bullet_area_enter(area):
 				area.life = 0
 			if super:
 				area.life = 0
-#					get_node("../ship").active = true
 		queue_free()
 
 func _on_visibility_exit_screen():
 	queue_free()
-
-func set_color(chosen_color):
-	for color in ["red","blue", "masked"]:
-		get_node(color).set_hidden(color != chosen_color)
