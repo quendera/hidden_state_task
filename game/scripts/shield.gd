d1a31080dabba0d7c774ad8e6f4e3f02c556da7f
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2017-06-20 09:19:12 +0100

    Before changing progressbar

--- game/scripts/shield.gd
+++ game/scripts/shield.gd
@@ -2,20 +2,15 @@ extends Area2D
 
 
 # class member variables go here, for example:
-var polarity = 1*(rand_range(0,1) > 0.5)
-var active = false
-const time_on = 1
-const time_off = 0.1
-var timer = 0
+var polarity
 const colors = ["red", "blue"]
 
 
 func _ready():
+	set_process(true)
+
+
+func _process(delta):
+	polarity = get_node("../").polarity
 	get_node("frames").set_frame(polarity)
-	set_process_input(true)
 
-
-func _input(event):
-	if event.is_action_pressed("polarity_change"):
-		polarity = 1-polarity
-		get_node("frames").set_frame(polarity)
