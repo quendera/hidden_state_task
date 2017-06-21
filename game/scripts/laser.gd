05c03319ed0802794d47372ab31d6d399116cc89
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2017-06-21 10:17:29 +0100

    maybe no swing

---
+++ game/scripts/laser.gd
@@ -0,0 +1,11 @@
+extends AnimatedSprite
+
+# class member variables go here, for example:
+var angular_velocity = 0.6
+
+
+func _ready():
+	pass#set_process(true)
+
+#func _process(delta):
+#	set_rot(get_rot()+delta*angular_velocity)
