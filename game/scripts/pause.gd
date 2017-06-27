ef8a91c069c324c72fcc5c8a522c5bdd18b89864
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2017-06-27 21:57:00 +0100

    solved bug

--- game/scripts/pause.gd
+++ game/scripts/pause.gd
@@ -1,9 +1,13 @@
 extends Node2D
 
+var is_paused = true
 
 func _ready():
+	get_tree().set_pause(true)
 	set_process_input(true)
 
 func _input(event):
 	if event.is_action_pressed("pause"):
-		get_tree().set_pause(not get_tree().is_paused())
+		is_paused = not is_paused
+		get_tree().set_pause(is_paused)
+		get_node("../CanvasLayer/shade").set_hidden(not is_paused)
