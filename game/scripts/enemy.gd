246a0fb7f4d5305b683ecebc1487595d4f85990a
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2017-07-07 11:38:22 +0100

    cleanup

--- game/scripts/enemy.gd
+++ game/scripts/enemy.gd
@@ -95,8 +95,6 @@ func _on_bullet_hit(steps):
 		consecutive = 0
 	else:
 		consecutive += 1
-	get_node("end_cue").start()
-
 
 func lose_life(steps, correct):
 	if correct:
@@ -129,18 +127,12 @@ func _on_anim_finished():
 	data_line["reaction_time"] = get_node("../ship").movement_time
 	for key in data_line.keys():
 		data[key].push_back(data_line[key])
-#	get_node("frames").get_material().set_shader_param("hidden", true)
-	var mask = rand_range(0,1)
-	get_node("frames").get_material().set_shader_param("x", 0.5*(polarity+mask))
-	get_node("end_cue").start()
+	get_node("frames").get_material().set_shader_param("hidden", true)
 	get_node("../ship").ready2shoot = true
 	get_node("../ship").charge = 0
 	get_node("lost_life").set_text("")
 	if life <= 0:
 		game_over()
-
-func _on_end_cue_timeout():
-	get_node("frames").get_material().set_shader_param("hidden",true)
 
 
 ### ATTACK ###
