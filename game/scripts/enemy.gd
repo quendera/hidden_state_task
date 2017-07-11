bbcfea102789da1905fa10480062164090788355
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2017-07-11 12:10:41 +0100

    works on phone

--- game/scripts/enemy.gd
+++ game/scripts/enemy.gd
@@ -33,26 +33,45 @@ var attack_frequency = [0.3, 0.03, 0.1]
 
 
 func _ready():
-	var HTTP = HTTPClient.new()
-	var url = "http://10.40.11.166/"
-	var RESPONSE = HTTP.connect("10.40.11.166",80)
+#	var HTTP = HTTPClient.new()
+#	var url = "http://10.40.11.166/var/www/"
+#	var RESPONSE = HTTP.connect("10.40.11.166",80)
+#	
+#	while(HTTP.get_status() == HTTPClient.STATUS_CONNECTING or HTTP.get_status() == HTTPClient.STATUS_RESOLVING):
+#		HTTP.poll()
+#		OS.delay_msec(300)
+#	assert(HTTP.get_status() == HTTPClient.STATUS_CONNECTED)
+#	var data = {"a":2, "b":3}
+#	var QUERY = HTTP.query_string_from_dict(data)
+#	var HEADERS = ["User-Agent: Pirulo/1.0 (Godot)", "Content-Type: application/x-www-form-urlencoded", "Content-Length: " + str(QUERY.length())]
+#	RESPONSE = HTTP.request(HTTPClient.METHOD_POST, url, HEADERS, QUERY)
+#	assert(RESPONSE == OK)
+#	
+#	while (HTTP.get_status() == HTTPClient.STATUS_REQUESTING):
+#		HTTP.poll()
+#		OS.delay_msec(300)
+    # Make sure request finished
+#	assert(HTTP.get_status() == HTTPClient.STATUS_BODY or HTTP.get_status() == HTTPClient.STATUS_CONNECTED)
+#	var RB = RawArray()
+#	var CHUNK = 0
+#	var RESULT = 0
+#	
+#	if HTTP.has_response():
+#		print("responded")
+#        # Get response headers
+#		var headers = HTTP.get_response_headers_as_dictionary()
+#		while(HTTP.get_status() == HTTPClient.STATUS_BODY):
+#			HTTP.poll()
+#			CHUNK = HTTP.read_response_body_chunk()
+#			if(CHUNK.size() == 0):
+#				OS.delay_usec(100)
+#			else:
+#				RB = RB + CHUNK
+#	HTTP.close()
+#	RESULT = RB.get_string_from_ascii()
+#	# Do something with the response
+#	print(str(RESULT))
 	
-	while(HTTP.get_status() == HTTPClient.STATUS_CONNECTING or HTTP.get_status() == HTTPClient.STATUS_RESOLVING):
-		HTTP.poll()
-		OS.delay_msec(300)
-	assert(HTTP.get_status() == HTTPClient.STATUS_CONNECTED)
-	var data = {"a":2, "b":3}
-	var QUERY = HTTP.query_string_from_dict(data)
-	var HEADERS = ["User-Agent: Pirulo/1.0 (Godot)", "Content-Type: application/x-www-form-urlencoded", "Content-Length: " + str(QUERY.length())]
-	RESPONSE = HTTP.request(HTTPClient.METHOD_POST, url, HEADERS, QUERY)
-	assert(RESPONSE == OK)
-	
-	while (HTTP.get_status() == HTTPClient.STATUS_REQUESTING):
-		HTTP.poll()
-		OS.delay_msec(300)
-#    # Make sure request finished
-	assert(HTTP.get_status() == HTTPClient.STATUS_BODY or HTTP.get_status() == HTTPClient.STATUS_CONNECTED)
-	print("testsuccess")
 	
 	randomize()
 	next_attack = randi() % 3
