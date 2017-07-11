fefb223254e32543b8333590d2e254258f335074
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2017-07-11 12:45:49 +0100

    figured post, now do php

--- game/scripts/enemy.gd
+++ game/scripts/enemy.gd
@@ -34,8 +34,10 @@ var attack_frequency = [0.3, 0.03, 0.1]
 
 func _ready():
 #	var HTTP = HTTPClient.new()
-#	var url = "http://10.40.11.166/var/www/"
-#	var RESPONSE = HTTP.connect("10.40.11.166",80)
+#	var url = "http://10.40.11.166/"
+#	url = "http://posttestserver.com/post.php"
+#	var RESPONSE = HTTP.connect("posttestserver.com",80)
+#	#var RESPONSE = HTTP.connect("10.40.11.166",80)
 #	
 #	while(HTTP.get_status() == HTTPClient.STATUS_CONNECTING or HTTP.get_status() == HTTPClient.STATUS_RESOLVING):
 #		HTTP.poll()
@@ -55,9 +57,8 @@ func _ready():
 #	var RB = RawArray()
 #	var CHUNK = 0
 #	var RESULT = 0
-#	
+#	print(str(HTTP.get_status() ))
 #	if HTTP.has_response():
-#		print("responded")
 #        # Get response headers
 #		var headers = HTTP.get_response_headers_as_dictionary()
 #		while(HTTP.get_status() == HTTPClient.STATUS_BODY):
@@ -71,8 +72,6 @@ func _ready():
 #	RESULT = RB.get_string_from_ascii()
 #	# Do something with the response
 #	print(str(RESULT))
-	
-	
 	randomize()
 	next_attack = randi() % 3
 	dir = Vector2(cos(rand_range(0,2*PI)),sin(rand_range(0,2*PI)))
